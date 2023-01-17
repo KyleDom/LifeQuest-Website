c5fab28bc179810f079cdfb66cf4235b3b4fe613
Ferjen Dave Torred <84013477+TheFerj@users.noreply.github.com>
2023-01-17 08:00:11 +0800

    Jenkinsfile

---
+++ Jenkinsfile.groovy
@@ -0,0 +1,22 @@
+pipeline{
+    agent any
+    stages {
+        stage('Pull') {
+            steps {
+         checkout scm(branches: [[name: '*/main']], extensions: [], userRemoteConfigs: [[url: 'https://github.com/KyleDom/LifeQuest-Website.git']])
+            }
+        }
+        stage('Docker Build') {
+    	agent any
+      steps {
+        dir('LifeQuest-Website/frontend'){
+        script{
+      	sh 'docker build -t ferjen/new:latest .'
+            
+        }    
+        }  
+      }
+    }
+        
+    }
+}
